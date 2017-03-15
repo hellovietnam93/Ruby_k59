733fbb59d5608eebd1031a2849f8a09722aa1887
TUNG LAM <coldwind1911@gmail.com>
2017-03-15 23:00:27 +0700

    TungLam Validate for Users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   get 'static_page/help'
 
-	root "static_page#home"
+  root "static_page#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
