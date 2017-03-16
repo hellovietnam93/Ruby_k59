b79b2baec6cfb73d66f5ec85b0e5d358b5d3d5b6
phamgiacuong <phamgiacuong266@gamil.com>
2017-03-16 11:28:05 +0700

    user

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+	validates :name, presence: true
+	validates :phone, numericality: true
+	validates
+	private
+	def check_length
+		if self.name && self.name.length<30
+			self.errors.add :name,"ten qua ngan"
+		end
+	end
+	def check_ngaysinh
+		t = Time.now
+		if self.ngaysinh.year<t.year-90 && self.ngaysinh.year>t.year-7
+			self.errors.add :ngaysinh,"khong hop tuoi"
+		end
+	end
+	def check_phone
+		unless self.phone.length==10 && self.phone[0]==0
+			self.errors.add :phone,"khong hop le"
+		end
+	end
+end
