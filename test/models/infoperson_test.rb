fc3da895f498043e506de114f26b5c06760aaf26
Nguyen Duc Chinh <Nguyenducchinh.hust@gmail.com>
2017-03-16 18:11:17 +0700

    Model_users

---
+++ test/models/infoperson_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class InfopersonTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/testinfo_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TestinfoTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
