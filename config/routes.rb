6046b886d6c94dba5e5c192416f5d449ecd75e02
TUNG LAM <coldwind1911@gmail.com>
2017-03-19 10:52:55 +0700

    Login Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  get 'static_page/home'
+  root "static_page#home"
+  get "/about", to: "static_page#about"
+  get "/blog", to: "static_page#blog"
+  get "/contact", to: "static_page#contact"
+  get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
-  get 'static_page/help'
 
-	root "static_page#home"
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
