a19d73717be648490d12812940e05035607e46ec
Quang <khanhbklc12@gmail.com>
2017-03-20 18:51:48 +0700

    add static page to user model
    sign up
    
    sign up + deleted folder static page
    
    readd
    
    fix
    
    fix second time
    
    fix time 3
    
    fix time 4
    
    fix time 5
    
    fix 6 time

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "static_pages/signup", to: "users#new"
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  root "static_pages#home"
+  resources :users
+
 end
