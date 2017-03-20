360074bfa8aa0a18924df9e27f9499754c6f930f
H2V <h2v9696@gmail>
2017-03-20 19:42:35 +0700

    Sign Up + Use SSL and the Puma webserver

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,33 +1,30 @@
-require 'date'
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :phone, presence: true
-
-  validate :check_length
+  before_save {email.downcase!}
+  validates :name, presence: true, length: {minimum: 10}
+  validates :phone, presence: true, length: {minimum: 10}
+  validates :birthday, presence: true
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validate :check_pnumber
   validate :check_years
 
+  has_secure_password
+  validates :password, presence: true, length: {minimum:6}
+
   private
+
   def check_pnumber
-      if self.phone && self.phone.length != 10
-        self.errors.add :phone, "Phone number must have 10 digit" #Add errors
-      end
-      if self.phone[0] != '0'
-        self.errors.add :phone, "Phone number have number 0 at first" #Add errors
-      end
+    if self.phone && self.phone[0] != "0"
+      self.errors.add :phone, "Phone number have number 0 at first"
+    end
   end
 
-  #private
   def check_years
-    if Time.now.year - self.birthday.year < 7 || Time.now.year - self.birthday.year > 90
-      self.errors.add :birthday, "User's age from 7 - 90 years old" #Add errors
+    years = Time.now.year - self.birthday.year
+    if self.birthday && (years < 7 || years > 90)
+      self.errors.add :birthday, "User's age from 7 - 90 years old"
     end
   end
 
-  #private
-  def check_length
-    if self.name && self.name.length < 10
-      self.errors.add :name, "Too short" #Add errors
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "static_pages#home"
+
+  get "/contact", to: "static_pages#contact"
+  get "/about", to: "static_pages#about"
+  get "/help", to: "static_pages#help"
+  get "/signup", to: "users#new"
+  post "/signup",  to: "users#create"
+  resources :users
 end
