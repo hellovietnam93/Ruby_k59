02efc405739fa4ce615c3d1d3bb5f2e776582eec
s2luckyboy <nguyenvanduong.vn.k59@gmail.com>
2017-03-20 18:54:17 +0700

    Sign up sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
 	root "users#index" # chuyen huong den users khi dang nhap vao trang chu
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
 end
