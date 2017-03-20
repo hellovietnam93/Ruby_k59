64b00089c88673a099821d29fa18867480d6fd1b
lampv318 <lampvhy@gmail.com>
2017-03-20 21:58:53 +0700

    new_page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do |n|
-  User.create name: "User #{n}", email: "example-#{n}@gmail.com", password: "123456",
-    password_confirmation: "123456"
-end
