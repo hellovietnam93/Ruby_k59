58eba342cdc3fa1ab9cb5842cb578077d7ee9b8b
TUNG LAM <coldwind1911@gmail.com>
2017-03-22 16:07:54 +0700

    Signup login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new user_params
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to My First Web!"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end
